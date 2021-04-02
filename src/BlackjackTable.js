e4dc463826e4c323ee37464151d2c89dc9ea60ff
Davia DeNisco <davia622@gmail.com>
2021-04-02 12:48:19 -0400

    set up basic table and card components

---
+++ src/BlackjackTable.js
@@ -0,0 +1,12 @@
+import Card from './Card';
+
+export default function BlackjackTable() {
+
+    return (
+        <div className='game-area'>
+            Blackjack Table
+            <Card />    
+        </div>
+    )
+}
+
